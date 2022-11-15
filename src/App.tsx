5b1e5a3765ec20aac2988c4f5e002271ced1af84
Curtis <curtis_102@hotmail.com>
2022-11-14 22:38:50 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider, Theme } from '@mui/material/styles';
 import './App.css';
+import MainMenu from './components/MainMenu/MainMenu';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Space Grotesk', 'sans-serif'].join(','),
+  },
+  palette: {
+    primary: {
+      light: '#7945FF',
+      main: '#5C2DD5',
+      dark: '#000',
+    },
+    secondary: {
+      light: '#FFF',
+      main: '#FFCE67',
+      dark: '#FD6687',
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: (theme: Theme) => ({
+        body: {
+          backgroundColor: theme.palette.primary.main,
+        },
+      }),
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+      },
+      styleOverrides: {
+        root: {
+          fontSize: 24,
+        },
+      },
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <MainMenu />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/MainMenu/MainMenu.styles.ts
@@ -0,0 +1,8 @@
+import { SxProps, Theme } from '@mui/material';
+
+export const MainMenuContainerStyle: SxProps<Theme> = (theme) => ({
+  bgcolor: 'primary.light',
+  px: 5,
+  pt: 8.75,
+  pb: 7.5,
+});
